0c1eaf2d6de39c7059860364de3db03134d80883
Isabella Pettersson <isabella.pettersso1@learnet.se>
2020-10-06 19:47:06 +0200

    created 2 XunitTests

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -11,6 +11,22 @@ namespace XUnitTestProject1
         public void Additions()
         {
             Assert.Equal(2, Conversion.Parse("one plus one"));
+            Assert.Equal(5, Conversion.Parse("two plus three"));
+            Assert.Equal(10, Conversion.Parse("five plus five"));
+            Assert.Equal(4, Conversion.Parse("three plus one"));
+            Assert.Throws<FormatException>(() => Conversion.Parse("one plus"));
+            Assert.Throws<FormatException>(() => Conversion.Parse("plus two"));
+        }
+
+        [Fact]
+        public void Subtractions()
+        {
+            Assert.Equal(3, Conversion.Parse("five minus two"));
+            Assert.Equal(-1, Conversion.Parse("one minus two"));
+            Assert.Equal(5, Conversion.Parse("six minus one"));
+            Assert.Equal(2, Conversion.Parse("eight minus six"));
+            Assert.Throws<FormatException>(() => Conversion.Parse("minus five"));
+            Assert.Throws<FormatException>(() => Conversion.Parse("nine minus"));
         }
     }
 }
