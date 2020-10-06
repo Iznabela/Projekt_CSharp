9487cd1480c84679e52e5f4b316f462e597a3061
Alirezam <Samrad84@yahoo.com>
2020-10-06 16:47:17 +0200

    Metoder för att ta in input och splitta den!

--- Projekt_CSharp/Program.cs
+++ Projekt_CSharp/Program.cs
@@ -6,8 +6,39 @@ namespace Projekt_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello There!");
+
+
+            Console.WriteLine("Write some math operation to translate (OBS : Avoid Digits! Just Text Please! ");
+            string firstinput = Console.ReadLine();
+            string[] words = firstinput.Split(' ');
+
+
+
+
+
+            foreach (var word in words)
+            {
+                double number = ConvertStringToDigit(word);
+                Console.WriteLine(number);
+
+
+            }
+
+
+
+
+        }
+        static double ConvertStringToDigit(string i)
+        {
+            switch (i)
+            {
+                case "Zero": return 0;
+                case "One": return 1;
+                default: return 10;
+
+
+            }
+
         }
     }
 }
